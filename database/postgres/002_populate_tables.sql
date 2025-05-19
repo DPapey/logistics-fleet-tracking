6c071a56539a6b38f3d1e4e6e92fed118115e2fb
dpapey <dpapey@outlook.com>
2025-05-19 23:09:15 +0100

    Fixed file structure, focus on PostgreSQL

--- database/postgres/002_populate_tables.sql
+++ database/postgres/002_populate_tables.sql
@@ -1,7 +1,13 @@
 use eurofleet_logistics;
 
-INSERT INTO powertrain_type (powertrain_type, powertrain_description) VALUES
+INSERT INTO powertrain (powertrain_type, powertrain_description) VALUES
 	('Diesel', 'Standard internal combustion diesel engine.'),
 	('Petrol', 'Internal combustion petrol/gasoline engine'),
 	('Electric', 'Fully electric powertrain.'),
 	('Hybrid', 'Electric and internal combustion.');
+
+
+INSERT INTO address_type (addr_type_name) VALUES
+	(''),
+	(''),
+	('');
